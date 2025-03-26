cbc5f0772d034522164c56cde099fb24ba2ced2a
ST10489659 <2019380@settlers.org.za>
2025-03-26 12:48:27 +0200

    Updated UI

--- app/src/main/java/za/co/varsitycollege/st10489659_assignment1nourix/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10489659_assignment1nourix/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             mealSuggestion.text = ""
         }
 
+
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
